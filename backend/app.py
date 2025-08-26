1fd816c449117ebae7d2fd74ff2229b3ca92a57e
Deepak Meena <84173190+deepakQE@users.noreply.github.com>
2025-08-26 23:50:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -191,7 +191,7 @@ def get_history():
 
 # Run --------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
